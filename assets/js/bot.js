3462fd2eae41d17e926b286c25899f06847d333d
Sudhanshu <2201030100@cgu-odisha.ac.in>
2023-12-27 11:06:46 +0530

    Changing the Bot detaiils

--- assets/js/bot.js
+++ assets/js/bot.js
@@ -50,16 +50,16 @@ sendButton.addEventListener("click", () => {
     addUserMessage(userMessage);
 
     // Process user input and provide responses
-    let response = "Hi, I'm Med Coders, an AI assistant.";
+    let response = "Hi, I'm Green Coders, an AI assistant.";
 
     switch (userMessage.toLowerCase()) {
         case "hello":
             response = "Hello! How can I assist you today?";
             break;
-        case "how are you?":
+        case "how are you":
             response = "I'm just a computer program, so I don't have feelings, but I'm here to help!";
             break;
-        case "what can you do?":
+        case "what can you do":
             response = "I can answer questions, provide information, and assist with various tasks. How can I assist you?";
             break;
         case "goodbye":
@@ -69,20 +69,20 @@ sendButton.addEventListener("click", () => {
             response = "To maintain a heart-healthy lifestyle, focus on a balanced diet rich in fruits, vegetables, whole grains, and lean proteins. Regular exercise is crucial; aim for at least 150 minutes of moderate-intensity aerobic activity per week.Manage stress through relaxation techniques like deep breathing and meditation.Avoid smoking and limit alcohol consumption.Regularly check your blood pressure, cholesterol levels, and maintain a healthy weight.Consult your healthcare provider for personalized advice and monitoring.Option 2: Explore Common Heart Disease Medications";
             break;
         case "mera naam":
-            response = "mera naam MEd coders hai";
+            response = "mera naam Green Coders hai";
             break;
             // Add more test cases and responses here
         case "i love you":
             response = "I love you too.";
             break;
         case "hi":
-            response = "Hi, I'm Med Coders, an AI assistant for Kiosk";
+            response = "Hi, I'm Green Coders, an AI assistant for Kiosk";
             break;
         case "नमस्ते":
             response = "मैं अच्छा हूँ,तुम कैसे हो?";
             break;
-        case "who is med coders":
-            response = "Hi, I'm Med Coders assistant";
+        case "who is Green Coders":
+            response = "Hi, I'm Green Coders assistant";
             break;
         case "What's the recommended daily intake of water for adults?":
             response = "The recommended daily water intake for adults varies but is typically around 8-10 glasses or 2-2.5 liters.";
