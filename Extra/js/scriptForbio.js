e1958b416dcfad8f634ffb5a8faeee24081361af
Sudhanshu <2201030100@cgu-odisha.ac.in>
2024-01-03 16:47:47 +0530

    Modify some things

--- Extra/js/scriptForbio.js
+++ Extra/js/scriptForbio.js
@@ -4,20 +4,16 @@ const imageModal = document.getElementById("image-modal");
 const loginForm = document.getElementById("login-form");
 
 
-// Show the login modal when the page loads
 window.addEventListener("load", () => {
     loginModal.style.display = "block";
 });
 
-// Show the biometric authentication modal
 loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    // You can add your biometric authentication logic here
-    // If authentication is successful, show the image modal
+
     bioAuthModal.style.display = "block";
 });
 
-// Close modals when the close button is clicked
 const closeModalButtons = document.querySelectorAll(".close");
 closeModalButtons.forEach((button) => {
     button.addEventListener("click", () => {
